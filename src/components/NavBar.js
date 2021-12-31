cd51a4b5bd7acd14fb62dba023f598b4a53e9a64
Kurtis Parsons <kuparsons24@gmail.com>
2021-12-31 10:37:15 -0500

    update navbar back button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,33 @@
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBar({ title, backButton }) {
+  let navigate = useNavigate();
+  
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <AppBar position="fixed" elevation={4} sx={{ color: 'white' }}>
       <Toolbar>
+        {backButton ?  
+        <IconButton onClick={handleBackClick} color='inherit' sx={{ paddingRight: '30px' }} >
+          <ArrowBackIosNewIcon />
+        </IconButton>
+        :         
+        null}
         <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          Habits
+          {title}
         </Typography>
+        {backButton ?  
+        null :         
         <IconButton color='inherit' >
           <AddIcon />
-        </IconButton>
+        </IconButton>}
       </Toolbar>
     </AppBar>
   );
